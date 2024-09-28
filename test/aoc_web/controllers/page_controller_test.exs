2334df32f6d1ad963f6f618d549a4b78e9498359
Jakub Sokołowski <jakub.sokolowski@sendfriend.io>
2024-09-28 22:52:49 +0200

    [#1] Fix tests after controller changes

--- test/aoc_web/controllers/page_controller_test.exs
+++ test/aoc_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule AocWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "No cześć co tam jak tam halo jak tam praca?"
   end
 end
