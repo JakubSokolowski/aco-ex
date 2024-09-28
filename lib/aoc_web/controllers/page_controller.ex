a9b90da02fd26eed9179929c23b198ca7c7271e8
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-09-28 22:18:50 +0200

    [#1] Initial commit

---
+++ lib/aoc_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule AocWeb.PageController do
+  use AocWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
