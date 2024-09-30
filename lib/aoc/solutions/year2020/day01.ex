44ba87922d92cd269e908392f67214f954e7ac23
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-10-01 00:11:57 +0200

    Add 2020/1 solution

--- lib/aoc/solutions/year2020/day01.ex
+++ lib/aoc/solutions/year2020/day01.ex
@@ -2,12 +2,69 @@ defmodule Aoc.Solutions.Year2020.Day01 do
   @behaviour Aoc.Solution
 
   @impl true
-  def silver(_input) do
-    "Silver"
+  def silver(input) do
+    numbers =
+      input
+      |> to_nums()
+
+    case find_pair_sum(numbers, 2020) do
+      {a, b} -> "#{a * b}"
+      _ -> "No pair found"
+    end
+  end
+
+  def to_nums(input) do
+    String.split(input, "\n")
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def find_pair_sum(nums, target) do
+    Enum.reduce_while(nums, MapSet.new(), fn num, seen ->
+      missing = target - num
+
+      if MapSet.member?(seen, missing) do
+        {:halt, {num, missing}}
+      else
+        {:cont, MapSet.put(seen, num)}
+      end
+    end)
+    |> case do
+      {_, _} = pair -> pair
+      _ -> nil
+    end
+  end
+
+  def find_triplet_sum(nums, target) do
+    pairs_lookup =
+      Enum.reduce(nums, Map.new(), fn num, lookup ->
+        new_target = target - num
+
+        case find_pair_sum(nums, new_target) do
+          {a, b} -> Map.put(lookup, new_target, {a, b})
+          _ -> lookup
+        end
+      end)
+
+    Enum.find_value(nums, fn num ->
+      new_target = target - num
+
+      case Map.get(pairs_lookup, new_target) do
+        {a, b} when a != num and b != num -> {num, a, b}
+        _ -> nil
+      end
+    end)
   end
 
   @impl true
-  def gold(_input) do
-    "Gold"
+  def gold(input) do
+    nums =
+      input
+      |> to_nums()
+
+    case find_triplet_sum(nums, 2020) do
+      {a, b, c} -> "#{a * b * c}"
+      _ -> "No triplet found"
+    end
   end
 end
