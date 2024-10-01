acd759f736bea3572efaaa4365d65db7927a28db
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-10-01 22:08:11 +0200

    Add solution for 2020/2

---
+++ lib/aoc/solutions/year2020/day02_test.exs
@@ -0,0 +1,28 @@
+defmodule Aoc.Solutions.Year2020.Day02Test do
+  use ExUnit.Case
+  alias Aoc.Solutions.Year2020.Day02
+
+  describe "silver/1" do
+    test "solves the silver challenge for a test input" do
+      input = """
+      1-3 a: abcde
+      1-3 b: cdefg
+      2-9 c: ccccccccc
+      """
+
+      assert Day02.silver(input) == 2
+    end
+  end
+
+  describe "gold/1" do
+    test "solves the gold challenge with a valid input" do
+      input = """
+      1-3 a: abcde
+      1-3 b: cdefg
+      2-9 c: ccccccccc
+      """
+
+      assert Day02.gold(input) == 1
+    end
+  end
+end
