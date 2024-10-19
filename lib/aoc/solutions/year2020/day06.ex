a30649589b68d160957215f947c054c8f9d64aa5
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-10-19 22:33:22 +0200

    Add 2020/6 solution

---
+++ lib/aoc/solutions/year2020/day06.ex
@@ -0,0 +1,49 @@
+defmodule Aoc.Solutions.Year2020.Day06 do
+  @behaviour Aoc.Solution
+
+  @impl true
+  def silver(input) do
+    input
+    |> parse_groups()
+    |> Enum.map(&count_anyone/1)
+    |> Enum.sum()
+  end
+
+  @impl true
+  def gold(input) do
+    input
+    |> parse_groups()
+    |> Enum.map(&count_everyone/1)
+    |> Enum.sum()
+  end
+
+  def parse_groups(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&String.split(&1, "\n", trim: true))
+  end
+
+  def count_anyone(group) do
+    group
+    |> count_answers()
+    |> Enum.map(fn {answer, _count} -> answer end)
+    |> Enum.count()
+  end
+
+  def count_everyone(group) do
+    group
+    |> count_answers()
+    |> Enum.filter(fn {_answer, count} -> count == length(group) end)
+    |> Enum.map(fn {answer, _count} -> answer end)
+    |> Enum.count()
+  end
+
+  def count_answers(group) do
+    group
+    |> Enum.join()
+    |> String.graphemes()
+    |> Enum.reduce(%{}, fn answer, acc ->
+      Map.update(acc, answer, 1, &(&1 + 1))
+    end)
+  end
+end
---
+++ lib/aoc/solutions/year2020/day06_test.ex
@@ -0,0 +1,66 @@
+defmodule Aoc.Solutions.Year2020.Day06Test do
+  use ExUnit.Case
+  import Elixir.Aoc.Solutions.Year2020.Day06
+
+  describe "silver/1" do
+    test "should solve silver for test input" do
+      input = """
+      abc
+
+      a
+      b
+      c
+
+      ab
+      ac
+
+      a
+      a
+      a
+      a
+
+      b
+      """
+
+      result = silver(input)
+
+      assert result == 11
+    end
+  end
+
+  describe "count_answers" do
+    test "should count every answer" do
+      input = ["ab", "ac"]
+
+      result = count_answers(input)
+
+      assert result
+    end
+  end
+
+  describe "gold/1" do
+    test "should solve gold for test input" do
+      input = """
+      abc
+
+      a
+      b
+      c
+
+      ab
+      ac
+
+      a
+      a
+      a
+      a
+
+      b
+      """
+
+      result = gold(input)
+
+      assert result == 6
+    end
+  end
+end
