a4d7fb477b41a10dbeb68642a5179bced90ed2fd
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-12-03 07:31:18 +0100

    Washed 2024-3

--- lib/aoc/solutions/year2024/day03.ex
+++ lib/aoc/solutions/year2024/day03.ex
@@ -1,80 +1,59 @@
 defmodule Aoc.Solutions.Year2024.Day03 do
   @behaviour Aoc.Solution
 
-  @mul_pattern ~r/mul\((\d+),(\d+)\)/
-  @do_pattern ~r/do\(\)/
-  @dont_pattern ~r/don't\(\)/
+  @patterns %{
+    mul: ~r/mul\((\d+),(\d+)\)/,
+    do: ~r/do\(\)/,
+    dont: ~r/don't\(\)/
+  }
 
   @impl true
   def silver(input) do
-    Regex.scan(@mul_pattern, input)
-    |> Enum.map(fn [_, l, r] -> String.to_integer(l) * String.to_integer(r) end)
+    @patterns.mul
+    |> Regex.scan(input)
+    |> Stream.map(fn [_, l, r] -> String.to_integer(l) * String.to_integer(r) end)
     |> Enum.sum()
   end
 
   @impl true
   def gold(input) do
-    muls =
-      Regex.scan(@mul_pattern, input, return: :index)
-      |> Enum.map(fn [{start_pos, length}, _, _] ->
-        match = String.slice(input, start_pos, length)
-        {match, start_pos, start_pos + length - 1}
-      end)
+    muls = find_matches(input, @patterns.mul)
+    dos = find_matches(input, @patterns.do)
+    donts = find_matches(input, @patterns.dont)
 
-    dos =
-      Regex.scan(@do_pattern, input, return: :index)
-      |> Enum.map(fn [{start_pos, length}] ->
-        match = String.slice(input, start_pos, length)
-        {match, start_pos, start_pos + length - 1}
-      end)
-
-    donts =
-      Regex.scan(@dont_pattern, input, return: :index)
-      |> Enum.map(fn [{start_pos, length}] ->
-        match = String.slice(input, start_pos, length)
-        {match, start_pos, start_pos + length - 1}
-      end)
-
-    valid_muls =
-      Enum.filter(muls, fn mul ->
-        not is_disabled(mul, dos, donts)
-      end)
-
-    sum =
-      valid_muls
-      |> Enum.map(fn {match, _, _} ->
-        [left, right] =
-          Regex.scan(~r/\d+/, match) |> List.flatten() |> Enum.map(&String.to_integer/1)
-
-        left * right
-      end)
-      |> Enum.sum()
-
-    sum
+    muls
+    |> Stream.reject(&disabled?(&1, dos ++ donts))
+    |> Stream.map(fn {match, _, _} ->
+      match
+      |> parse_numbers()
+      |> Enum.reduce(&*/2)
+    end)
+    |> Enum.sum()
   end
 
-  def is_disabled(mul, dos, donts) do
-    all = Enum.sort_by(dos ++ donts, fn {_, start, _} -> start end)
-    {_, mul_start, _} = mul
+  defp find_matches(input, pattern) do
+    pattern
+    |> Regex.scan(input, return: :index)
+    |> Enum.map(fn [{start, len} | _] ->
+      {String.slice(input, start, len), start, start + len - 1}
+    end)
+  end
 
-    prevs =
-      all
-      |> Enum.filter(fn {_, start, _} -> start < mul_start end)
+  defp parse_numbers(str) do
+    ~r/\d+/
+    |> Regex.scan(str)
+    |> List.flatten()
+    |> Enum.map(&String.to_integer/1)
+  end
 
-    closest_prev = prevs |> List.last()
-
-    disabled =
-      case closest_prev do
-        nil ->
-          false
-
-        {match, _, _} ->
-          case match do
-            "do()" -> false
-            "don't()" -> true
-          end
-      end
-
-    disabled
+  defp disabled?({_, mul_start, _}, controls) do
+    controls
+    |> Enum.sort_by(fn {_, start, _} -> start end)
+    |> Enum.take_while(fn {_, start, _} -> start < mul_start end)
+    |> List.last()
+    |> case do
+      {"don't()", _, _} -> true
+      _ -> false
+    end
   end
 end
---
+++ test/aoc/solutions/year2024/day03_test.ex
@@ -0,0 +1,40 @@
+defmodule Aoc.Solutions.Year2024.Day03Test do
+  use ExUnit.Case
+  import Elixir.Aoc.Solutions.Year2024.Day03
+
+  @year 2024
+  @day 3
+  @test_input "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+  describe "silver/1" do
+    test "should solve silver for test input" do
+      result = silver(@test_input)
+
+      assert result == 161
+    end
+
+    test "should solve silver for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = silver(input)
+
+      assert result == 184_511_516
+    end
+  end
+
+  describe "gold/1" do
+    test "should solve gold for test input" do
+      result = gold(@test_input)
+
+      assert result == 48
+    end
+
+    test "should solve gold for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = gold(input)
+
+      assert result == 90_044_227
+    end
+  end
+end
