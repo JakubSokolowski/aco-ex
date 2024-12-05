f2c29670e979644396266863efc271925cd479c5
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-12-05 16:14:14 +0100

    Wash and move tests to the proper dir

--- lib/aoc/solutions/year2024/day05.ex
+++ lib/aoc/solutions/year2024/day05.ex
@@ -8,14 +8,10 @@ defmodule Aoc.Solutions.Year2024.Day05 do
       |> String.split("\n\n", trim: true)
       |> then(fn [rules, updates] -> [parse_rules(rules), parse_updates(updates)] end)
 
-    middles =
-      updates
-      |> Enum.filter(fn update -> updated_valid?(rules, update) end)
-      |> Enum.map(fn update -> get_middle(update) end)
-
-    dbg(middles)
-
-    middles |> Enum.sum()
+    updates
+    |> Enum.filter(fn update -> update_valid?(rules, update) end)
+    |> Enum.map(fn update -> get_middle(update) end)
+    |> Enum.sum()
   end
 
   @impl true
@@ -25,25 +21,13 @@ defmodule Aoc.Solutions.Year2024.Day05 do
       |> String.split("\n\n", trim: true)
       |> then(fn [rules, updates] -> [parse_rules(rules), parse_updates(updates)] end)
 
-    dbg(rules)
-
-    incorrect =
-      updates
-      |> Enum.filter(fn update -> not updated_valid?(rules, update) end)
-
-    dbg(incorrect)
-
-    sorted =
-      incorrect
-      |> Enum.map(fn update ->
-        Enum.sort(update, fn a, b ->
-          MapSet.member?(rules, {a, b})
-        end)
+    updates
+    |> Enum.filter(fn update -> not update_valid?(rules, update) end)
+    |> Enum.map(fn update ->
+      Enum.sort(update, fn a, b ->
+        MapSet.member?(rules, {a, b})
       end)
-
-    dbg(sorted)
-
-    sorted
+    end)
     |> Enum.map(fn update -> get_middle(update) end)
     |> Enum.sum()
   end
@@ -52,7 +36,7 @@ defmodule Aoc.Solutions.Year2024.Day05 do
     Enum.at(list, div(length(list), 2))
   end
 
-  def updated_valid?(rules, update) do
+  def update_valid?(rules, update) do
     update
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.all?(fn [first, second] -> pair_valid?(rules, first, second) end)
@@ -60,37 +44,6 @@ defmodule Aoc.Solutions.Year2024.Day05 do
 
   def pair_valid?(rules, first, second) do
     MapSet.member?(rules, {first, second})
-  end
-
-  def sort(list, rules) when is_list(list) do
-    do_sort(list, length(list), rules)
-  end
-
-  defp do_sort(list, n, _rules) when n <= 1, do: list
-
-  defp do_sort(list, n, rules) do
-    {new_list, swapped} = do_iteration(list, rules)
-
-    case swapped do
-      true -> do_sort(new_list, n - 1, rules)
-      false -> new_list
-    end
-  end
-
-  defp do_iteration(list, rules) do
-    list
-    |> Enum.reduce({[], false}, fn
-      elem, {[], _swapped} ->
-        {[elem], false}
-
-      elem, {[head | tail], swapped} ->
-        if pair_valid?(rules, elem, head) do
-          {[head | [elem | tail]], swapped}
-        else
-          {[elem, head | tail], true}
-        end
-    end)
-    |> then(fn {list, swapped} -> {Enum.reverse(list), swapped} end)
   end
 
   def parse_rules(rules) do
---
+++ test/aoc/solutions/year2024/day05_test.exs
@@ -0,0 +1,67 @@
+defmodule Aoc.Solutions.Year2024.Day05Test do
+  use ExUnit.Case
+  import Elixir.Aoc.Solutions.Year2024.Day05
+
+  @test_input """
+  47|53
+  97|13
+  97|61
+  97|47
+  75|29
+  61|13
+  75|53
+  29|13
+  97|29
+  53|29
+  61|53
+  97|53
+  61|29
+  47|13
+  75|47
+  97|75
+  47|61
+  75|61
+  47|29
+  75|13
+  53|13
+
+  75,47,61,53,29
+  97,61,53,29,13
+  75,29,13
+  75,97,47,61,53
+  61,13,29
+  97,13,75,29,47
+  """
+
+  describe "silver/1" do
+    test "should solve silver for test input" do
+      result = silver(@test_input)
+
+      assert result == 143
+    end
+
+    test "should solve silver for real input" do
+      input = Aoc.Solver.fetch_input(2024, 5)
+
+      result = silver(input)
+
+      assert result == 6384
+    end
+  end
+
+  describe "gold/1" do
+    test "should solve gold for test input" do
+      result = gold(@test_input)
+
+      assert result == 123
+    end
+
+    test "should solve gold for real input" do
+      input = Aoc.Solver.fetch_input(2024, 5)
+
+      result = gold(input)
+
+      assert result == 5353
+    end
+  end
+end
