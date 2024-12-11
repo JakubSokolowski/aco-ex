d902fa2b96a1b071e4a53431266620b54127542e
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-12-11 18:18:22 +0100

    Add 2024 11 solution

---
+++ lib/aoc/solutions/year2024/day11.ex
@@ -0,0 +1,72 @@
+defmodule Aoc.Solutions.Year2024.Day11 do
+  @behaviour Aoc.Solution
+
+  @impl true
+  def silver(input) do
+    input
+    |> parse_counts()
+    |> blink_times(25)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  @impl true
+  def gold(input) do
+    input
+    |> parse_counts()
+    |> blink_times(75)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  def blink_times(counts, n) do
+    Enum.reduce(1..n, counts, fn _i, acc -> blink(acc) end)
+  end
+
+  def stone_counts(stones) do
+    Enum.reduce(stones, %{}, fn stone, acc -> acc |> Map.update(stone, 1, &(&1 + 1)) end)
+  end
+
+  def count_digits(num) do
+    num |> Integer.to_string() |> String.length()
+  end
+
+  def blink(counts) do
+    Enum.reduce(counts, %{}, fn {num, count}, acc ->
+      cond do
+        count_digits(num) |> rem(2) == 0 ->
+          {first, second} = split_num(num)
+
+          acc
+          |> Map.update(first, count, &(&1 + count))
+          |> Map.update(second, count, &(&1 + count))
+
+        num == 0 ->
+          acc |> Map.update(1, count, &(&1 + count))
+
+        true ->
+          acc |> Map.update(num * 2024, count, &(&1 + count))
+      end
+    end)
+  end
+
+  def split_num(num) do
+    num_str = Integer.to_string(num)
+    len = String.length(num_str)
+
+    num_str
+    |> String.split_at(div(len, 2))
+    |> then(fn {first, second} ->
+      {String.to_integer(first), String.to_integer(second)}
+    end)
+  end
+
+  def parse_counts(input) do
+    input
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(%{}, fn num, acc ->
+      Map.put(acc, num, 1)
+    end)
+  end
+end
---
+++ test/aoc/solutions/year2024/day11_test.exs
@@ -0,0 +1,44 @@
+defmodule Aoc.Solutions.Year2024.Day11Test do
+  use ExUnit.Case
+  import Elixir.Aoc.Solutions.Year2024.Day11
+
+  @day 11
+  @year 2024
+  @test_input "125 17"
+
+  describe "silver/1" do
+    test "should solve silver for test input" do
+      input = @test_input
+
+      result = silver(input)
+
+      assert result == 55312
+    end
+
+    test "should solve silver for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = silver(input)
+
+      assert result == 188_902
+    end
+  end
+
+  describe "gold/1" do
+    test "should solve gold for test input" do
+      input = @test_input
+
+      result = gold(input)
+
+      assert result == 65_601_038_650_482
+    end
+
+    test "should solve gold for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = gold(input)
+
+      assert result == 223_894_720_281_135
+    end
+  end
+end
