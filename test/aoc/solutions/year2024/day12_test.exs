4c1c3eee280f2d7679de69325946bf49e1c1d6ca
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-12-12 20:54:35 +0100

    Add 2024-12 solution (washed some)

---
+++ test/aoc/solutions/year2024/day12_test.exs
@@ -0,0 +1,98 @@
+defmodule Aoc.Solutions.Year2024.Day12Test do
+  use ExUnit.Case
+  import Elixir.Aoc.Solutions.Year2024.Day12
+
+  alias Aoc.Solutions.Grid
+
+  @day 12
+  @year 2024
+  @test_input """
+  RRRRIICCFF
+  RRRRIICCCF
+  VVRRRCCFFF
+  VVRCCCJFFF
+  VVVVCJJCFE
+  VVIVCCJJEE
+  VVIIICJJEE
+  MIIIIIJJEE
+  MIIISIJEEE
+  MMMISSJEEE
+  """
+
+  @xoxo_input """
+  OOOOO
+  OXOXO
+  OOOOO
+  OXOXO
+  OOOOO
+  """
+
+  @fat_input """
+  OOOOO
+  OOOOO
+  OOOOO
+  OXOXO
+  OOOOO
+  """
+
+  @min_input """
+  AAAAAA
+  AAABBA
+  AAABBA
+  ABBAAA
+  ABBAAA
+  AAAAAA
+  """
+
+  describe "silver/1" do
+    test "should solve silver for test input" do
+      input = @test_input
+
+      result = silver(input)
+
+      assert result == 1930
+    end
+
+    test "should solve silver for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = silver(input)
+
+      assert result == 1_522_850
+    end
+  end
+
+  describe "gold/1" do
+    test "should solve gold for test input" do
+      input = @test_input
+
+      result = gold(input)
+
+      assert result == 1206
+    end
+
+    test "should solve gold for min input" do
+      input = @min_input
+
+      result = gold(input)
+
+      assert result == 368
+    end
+
+    test "should solve gold for xoxo input" do
+      input = @xoxo_input
+
+      result = gold(input)
+
+      assert result == 436
+    end
+
+    test "should solve gold for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = gold(input)
+
+      assert result == 953_738
+    end
+  end
+end
