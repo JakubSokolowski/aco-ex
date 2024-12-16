0b4e32fbae3b13bb879488aeed47828c00a32828
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-12-16 08:14:41 +0100

    2024-16 longgg

---
+++ test/aoc/solutions/year2024/day16_test.exs
@@ -0,0 +1,63 @@
+defmodule Aoc.Solutions.Year2024.Day16Test do
+  use ExUnit.Case
+  import Elixir.Aoc.Solutions.Year2024.Day16
+
+  @day 16
+  @year 2024
+  @test_input """
+  #################
+  #...#...#...#..E#
+  #.#.#.#.#.#.#.#.#
+  #.#.#.#...#...#.#
+  #.#.#.#.###.#.#.#
+  #...#.#.#.....#.#
+  #.#.#.#.#.#####.#
+  #.#...#.#.#.....#
+  #.#.#####.#.###.#
+  #.#.#.......#...#
+  #.#.###.#####.###
+  #.#.#...#.....#.#
+  #.#.#.#####.###.#
+  #.#.#.........#.#
+  #.#.#.#########.#
+  #S#.............#
+  #################
+  """
+
+  describe "silver/1" do
+    test "should solve silver for test input" do
+      input = @test_input
+
+      cost = silver(input)
+
+      assert cost == 11048
+    end
+
+    @tag timeout: :infinity
+    test "should solve silver for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = silver(input)
+
+      assert result == 83444
+    end
+  end
+
+  describe "gold/1" do
+    test "should solve gold for test input" do
+      input = @test_input
+
+      result = gold(input)
+
+      assert result == 64
+    end
+
+    test "should solve gold for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = gold(input)
+
+      assert result == 478
+    end
+  end
+end
