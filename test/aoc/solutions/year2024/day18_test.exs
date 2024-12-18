f816482d2ac8646ef5bb19171681942d2c6d955d
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2024-12-18 06:53:26 +0100

    Add day 16, ekhm, i mean day 18 solution

---
+++ test/aoc/solutions/year2024/day18_test.exs
@@ -0,0 +1,27 @@
+defmodule Aoc.Solutions.Year2024.Day18Test do
+  use ExUnit.Case
+  import Elixir.Aoc.Solutions.Year2024.Day18
+
+  @day 18
+  @year 2024
+
+  describe "silver/1" do
+    test "should solve silver for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = silver(input)
+
+      assert result == 322
+    end
+  end
+
+  describe "gold/1" do
+    test "should solve gold for real input" do
+      input = Aoc.Solver.fetch_input(@year, @day)
+
+      result = gold(input)
+
+      assert result == {60, 21}
+    end
+  end
+end
